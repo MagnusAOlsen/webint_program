118e38749dcb24ce1f981818d139d49ca505e9b5
marenfj <marenf.johansen@gmail.com>
2026-05-11 21:43:16 +0200

    added wineOfTheDay logic

--- src/javascript/homepage.js
+++ src/javascript/homepage.js
@@ -1,3 +1,7 @@
+let today = getTodayString();
+let x = 1;
+let wineOfTheDay;
+
 fetchReviews().then(reviews => {
 
     reviews.sort((a, b) => {
@@ -6,7 +10,11 @@ fetchReviews().then(reviews => {
         return dateB - dateA; 
     });
 
+    wineOfTheDay = reviews.find(u => u.id === x);
+
     displayReviews(reviews, '#wineReviewImages', 4);
+
+    displayWineOfTheDay(getTodayString(), reviews);
 });
 
 function convertDate(dateString) {
@@ -14,5 +22,23 @@ function convertDate(dateString) {
     return new Date(`${year}-${month}-${day}`);
 }
 
+function getTodayString() {
+    return new Date().toISOString().split("T")[0];
+}
 
+
+function displayWineOfTheDay(currentDate, reviews) {
+    if (currentDate !== today) {
+        if (x < reviews.length) {
+            x += 1;
+        } else {
+            x = 1;
+        }
+        
+        today = currentDate;
+        wineOfTheDay = reviews.find(u => u.id === x);
+    }
+
+    displayReviews([wineOfTheDay], '#wineOfTheDay', 1);
+}
 
